8bf8951365e736e9856d88bb2730659100a472b5
aproschay <a.proschay@yandex.ru>
2023-07-15 13:04:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+/*ЗАДАНИЕ 1
+
+let a = 10;
+alert(a); 
+a = 20;
+alert(a);
+*/
+
+/*ЗАДАНИЕ 2
+
+let firstiPhoneYear = (' 2007');
+alert('Год выпуска первого Iphone:' + firstiPhoneYear);
+*/
+
+/*ЗАДАНИЕ 3
+
+let nameCreator = (' Брендан Эйх');
+alert(nameCreator)
+*/
+
+/*ЗАДАНИЕ 4
+
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x / y);
+alert(x * y);
+*/
+
+/*ЗАДАНИЕ 5 
+
+let a = 2 ** 5;
+alert(a)
+*/
+
+/*ЗАДАНИЕ 6
+
+let a = 9;
+let b = 2;
+alert(a % b);
+*/
+
+/*ЗАДАНИЕ 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);*/
+
+/*ЗАДАНИЕ 8
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+*/
+
+/*ЗАДАНИЕ 9.0
+user = {
+    name: "Nastya",
+    age: 20,
+    isAdmin: true,
+}*/
+
+/*ЗАДАНИЕ 9.1
+
+let user.cityOfResidence = { Belgorod };
+*/
+
+/*ЗАДАНИЕ 9.2
+
+let age = 19;
+*/
+
+/*ЗАДАНИЕ 9.3
+
+delete user.cityOfResidence;
+*/
+
+/*ЗАДАНИЕ 9.4
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+console.log(user[info]);
+alert(user);
+*/
+
+/*ЗАДАНИЕ 10
+
+let name = prompt('Как вас зовут?');
+alert('Привет, ' + name + '!'); 
+*/
