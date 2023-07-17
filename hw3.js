61126e6d83308e818ba0a89bd5cedb6edb1e8aae
Anastasia <an.proschay@yandex.ru>
2023-07-17 21:54:41 +0300

    hw3_1

---
+++ hw3.js
@@ -0,0 +1,82 @@
+/*ЗАДАНИЕ 1
+let password = 'qwerty';
+let question = prompt('Введите пароль');
+if (password === true) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}*/
+
+
+/*ЗАДАНИЕ 2
+
+let c = 15;
+if (c >= 0 && c <= 10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}*/
+
+
+/*ЗАДАНИЕ 3
+
+let d = 16
+let e = 10
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}*/
+
+/*ЗАДАНИЕ 4
+let a = '2';
+let b = '3';
+alert (Number(a) + Number(b));
+*/
+
+/*ЗАДАНИЕ 5
+
+let monthNumber = prompt ('Введине номер месяца');
+
+switch (monthNumber) {
+    case '1':
+        console.log(Зима);
+        break;
+    case '2':
+        console.log(Зима);
+        break;
+    case '3':
+        console.log(Весна);
+        break;
+    case '4':
+        console.log(Весна);
+        break;
+    case '5':
+        console.log(Весна);
+        break;
+    case '6':
+        console.log(Лето);
+        break;
+    case '7':
+        console.log(Лето);
+        break;
+    case '8':
+        console.log(Лето);
+        break;
+    case '9':
+        console.log(Осень);
+        break;    
+    case '10':
+        console.log(Осень);
+        break;    
+    case '11':
+        console.log(Осень);
+        break;
+    case '12':
+        console.log(Зима);
+        break;
+    default:
+        console.log('Ты ввел что-то не то');
+        break;
+}
+*/
