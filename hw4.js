7c6536fc9b4ba235b2cfdadc68c5016f88eb7183
Anastasia <an.proschay@yandex.ru>
2023-07-31 19:49:03 +0300

    №4.1

---
+++ hw4.js
@@ -0,0 +1,58 @@
+/* ЗАДАНИЕ 1
+let i = 2;
+
+for (let i = 0; i < 2; i++) {
+
+    console.log ("Привет");
+} */
+
+ /*ЗАДАНИЕ 2
+
+ let a = 1, b = 5;
+
+ while (a <= b) {
+    console.log(a);
+    a +=1; 
+ } */
+
+ /* ЗАДАНИЕ 3
+ 
+ let c = 7, e = 22;
+
+ while (c <= e) {
+    console.log(c);
+    c +=7; 
+ } */
+
+ /* ЗАДАНИЕ 4
+
+  let obj = {'Kolya': 200, 'Vasya': 300, 'Petya': 400};
+
+  for (key in obj) {
+    document.write([key] + ' - Зарплата' + obj[key] + 'долларов' + '<br>')
+  }
+  */
+
+  /*ЗАДАНИЕ 5
+   let n = 1000;
+   let num = 0;
+   while (n > 50) {
+    num ++;
+    n /= 2; 
+   }
+
+   document.write();(n);
+   document.write();(num);
+
+*/
+
+//ЗАДАНИЕ 6
+
+let firstFridayOfMonth = 1;
+let monthStartDay = 1;
+
+while (firstFridayOfMonth + ((monthStartDay - 1) % 7)) {
+    console.log ()
+}
+
+
